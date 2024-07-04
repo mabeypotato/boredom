77ad96aadc7c6f1cde5267089c27004624cdf8fd
Connor Mabey <connormabey@gmail.com>
2024-07-04 10:26:00 -0600

    figure out plotting issue. it was so much easier lol

--- gorge.m
+++ gorge.m
@@ -1,37 +1,16 @@
 clear, clc, close all;
 
-%% setup
+%% import dted data and plot 
 
-grid.latlimitlower = 30;
-grid.latlimitupper = 31;
+[grid_3, R3] = readgeoraster("n30_e110_1arc_v3.dt2", "OutputType","double"); % bottom left
+[grid_4, R4] = readgeoraster("n30_e111_1arc_v3.dt2", "OutputType","double"); % bottom right
+[grid_2, R2] = readgeoraster("n31_e110_1arc_v3.dt2", "OutputType","double"); % top left
+[grid_1, R1] = readgeoraster("n31_e111_1arc_v3.dt2", "OutputType","double"); % top right
 
-grid.longlimitlower = 110;
-grid.longlimitupper = 111;
-
-grid.totallength = 3601 * 2;
-grid.totalwidth = 3601 * 2;
-
-
-%% import dted data
-
-[grid_1, R] = readgeoraster("n30_e110_1arc_v3.dt2", "OutputType","double"); % bottom left
-[grid_2, R] = readgeoraster("n30_e111_1arc_v3.dt2", "OutputType","double"); % bottom right
-[grid_3, R] = readgeoraster("n31_e110_1arc_v3.dt2", "OutputType","double"); % top left
-[grid_4, R] = readgeoraster("n31_e111_1arc_v3.dt2", "OutputType","double"); % top right
-
-%% combine dteds
-
-grid_total = zeros([grid.totallength, grid.totalwidth]);
-
-grid_total(1:3601, 1:3601) = grid_3; % top left
-grid_total(1:3601, 3602:7202) = grid_4; % top right
-grid_total(3602:7202, 1:3601) = grid_2; % bottom left
-grid_total(3602:7202, 3602:7202) = grid_1; % bottom right
-
-%% plot them
-
-latlim = R.LatitudeLimits;
-lonlim = R.LongitudeLimits;
-worldmap(latlim, lonlim);
-geoshow(grid_total,R, "DisplayType", "surface");
-demcmap(grid_total);
+[big_grid, RA] = mergetiles(grid_1, R1, grid_2, R2, grid_3, R3, grid_4, R4);
+%%
+figure;
+worldmap(big_grid, RA);
+geoshow(big_grid, RA, 'DisplayType', 'surface')
+demcmap(big_grid);
+title('a really cool place for a B-21 training flight');
